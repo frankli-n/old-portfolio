dbb15316d7676a2820488d3eb1126f4e1399592b
frankli-n <frankiehaubrich@gmail.com>
2020-11-30 12:28:39 -0800

    a

--- memsmosaics/filter/filterable-gallery/dist/script.js
+++ memsmosaics/filter/filterable-gallery/dist/script.js
@@ -50,3 +50,26 @@ $("#gallery-header-center-left-icon").click(function() { if(size==0){size=1;}els
 check_button();
 check_size();
 });
+
+
+
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
